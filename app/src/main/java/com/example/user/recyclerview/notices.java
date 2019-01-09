b6ffe1a897c1afd9435fcbc0e9a2430251774b09
tclubmckvie <tclubmckvie@gmail.com>
2019-01-09 12:12:36 +0530

    first commit

---
+++ app/src/main/java/com/example/user/recyclerview/notices.java
@@ -0,0 +1,13 @@
+package com.example.user.recyclerview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class notices extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notices);
+    }
+}
